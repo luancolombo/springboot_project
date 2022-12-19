8605433c8589bd99652359d42c2b7922210415a1
Luan Colombo <luanbelly@gmail.com>
2022-12-19 17:57:39 +0000

    User insert

--- src/main/java/com/firstSpringProject/Spring/resources/UserResource.java
+++ src/main/java/com/firstSpringProject/Spring/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.firstSpringProject.Spring.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.firstSpringProject.Spring.entities.User;
 import com.firstSpringProject.Spring.services.UserService;
@@ -15,21 +19,27 @@ import com.firstSpringProject.Spring.services.UserService;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	private ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
 }
--- src/main/java/com/firstSpringProject/Spring/services/UserService.java
+++ src/main/java/com/firstSpringProject/Spring/services/UserService.java
@@ -24,5 +24,9 @@ public class UserService {
 		return obj.get();
 	}
 	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
 
 }
